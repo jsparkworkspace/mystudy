6871d33a6bc7a236ea00ca6f71e11d4714109b07
jsparkworkspace <wnstn_js@naver.com>
2023-11-22 20:40:24 +0900

    2023-11-22

--- java-basic/app/src/main/java/com/eomcs/lang/ex03/Exam0100.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Exam0100.java
@@ -1,31 +1,40 @@
 package com.eomcs.lang.ex03;
 
-//# 리터럴(literal)
-//- 자바 언어로 표현한 값.
+// # 리터럴(literal)
+// - 자바 언어로 표현한 값.
 //
 
 public class Exam0100 {
   public static void main(String[] args) {
     System.out.println("-------- 정수 리터럴");
     System.out.println(78); // 10진수
-    System.out.println(+78);
+    System.out.println(+78); // 정수 리터럴에서의 + 는 양수를 나타내는 부호로 해석되어 결과에는 출력되지 않음
     System.out.println(-78);
     System.out.println(0116); // 8진수
     System.out.println(0x4e); // 16진수
     System.out.println(0b01001110); // 2진수
-    
+
     System.out.println("-------- 부동소수점 리터럴");
     System.out.println(3.14);
     System.out.println(31.4e-1);
-    
+
+
     System.out.println("-------- 논리 리터럴");
     System.out.println(true);
     System.out.println(false);
-    
+
     System.out.println("-------- 문자 리터럴");
     System.out.println('가');
-    
+
     System.out.println("-------- 문자열 리터럴");
     System.out.println("오호라 코딩스쿨!");
+
+    System.out.println("-------- 헷갈릴 수 있음");
+    System.out.println("test" + 78);
+    System.out.println(78 + "test");
+    // 여기서 + 는 문자열과 숫자를 결합하는 연산으로 사용
+    // 숫자 78이 문자열로 형변환되어 문자열 "test78", "78test"가 출력됨
+
+
   }
 }
--- java-basic/app/src/main/java/com/eomcs/lang/ex03/Exam0610.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Exam0610.java
@@ -1,11 +1,11 @@
 package com.eomcs.lang.ex03;
 
-//# 문자열 리터럴
-//- 자바의 기본 타입이 아니라 객체이다.
-//- 객체란? 여러 데이터들의 덩어리이다.
-//- 표기법
-//  큰 따옴표(double quote) 안에 문자들(문자열)을 작성한다.
-//  예) "홍길동", "임꺽정", "hello"
+// # 문자열 리터럴
+// - 자바의 기본 타입이 아니라 객체이다.
+// - 객체란? 여러 데이터들의 덩어리이다.
+// - 표기법
+// 큰 따옴표(double quote) 안에 문자들(문자열)을 작성한다.
+// 예) "홍길동", "임꺽정", "hello"
 //
 public class Exam0610 {
   public static void main(String[] args) {
@@ -14,6 +14,9 @@ public class Exam0610 {
 
     // 문자열에 유니코드를 포함할 수 있다.
     System.out.println("\u0041BC\uac00나다");
+
+    // \\는 \를 출력하기 때문에 문자열에 유니코드가 포함되지 않는다.
+    System.out.println("\\u0041BC");
   }
 }
 
