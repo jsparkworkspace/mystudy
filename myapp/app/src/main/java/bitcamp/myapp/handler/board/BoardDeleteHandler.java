a4e9c0e43a52009567bcf2764d824cf4bef95902
jsparkworkspace <wnstn_js@naver.com>
2023-12-13 10:08:52 +0900

    20231213

--- myapp/app/src/main/java/bitcamp/myapp/handler/board/BoardDeleteHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/board/BoardDeleteHandler.java
@@ -6,9 +6,6 @@ import bitcamp.util.AnsiEscape;
 import bitcamp.util.ObjectRepository;
 import bitcamp.util.Prompt;
 
-// 게시글의 '등록' 메뉴를 선택했을 때 작업을 수행하는 클래스
-// - 반드시 MenuHandler 규칙에 따라 클래스를 작성해야 한다.
-//
 public class BoardDeleteHandler implements MenuHandler {
 
   ObjectRepository objectRepository;
@@ -27,14 +24,5 @@ public class BoardDeleteHandler implements MenuHandler {
     if (this.objectRepository.remove(index) == null) {
       System.out.println("게시글 번호가 유효하지 않습니다.");
     }
-//    if (index < 0 || index >= this.boardRepository.length) {
-//      System.out.println("게시글 번호가 유효하지 않습니다.");
-//      return;
-//    }
-
-//    for (int i = index; i < (this.boardRepository.length - 1); i++) {
-//      this.boardRepository.boards[i] = this.boardRepository.boards[i + 1];
-//    }
-//    this.boardRepository.boards[--this.boardRepository.length] = null;
   }
 }
--- myapp/app/src/main/java/bitcamp/myapp/handler/board/BoardModifyHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/board/BoardModifyHandler.java
@@ -7,9 +7,6 @@ import bitcamp.util.AnsiEscape;
 import bitcamp.util.ObjectRepository;
 import bitcamp.util.Prompt;
 
-// 게시글의 '등록' 메뉴를 선택했을 때 작업을 수행하는 클래스
-// - 반드시 MenuHandler 규칙에 따라 클래스를 작성해야 한다.
-//
 public class BoardModifyHandler implements MenuHandler {
 
   ObjectRepository objectRepository;
@@ -30,12 +27,7 @@ public class BoardModifyHandler implements MenuHandler {
       System.out.println("게시글 번호가 유효하지 않습니다.");
       return;
     }
-//    if (index < 0 || index >= this.boardRepository.length) {
-//      System.out.println("게시글 번호가 유효하지 않습니다.");
-//      return;
-//    }
 
-//    Board board = this.boardRepository.boards[index];
     Board board = new Board();
     board.title = this.prompt.input("제목(%s)? ", oldBoard.title);
     board.content = this.prompt.input("내용(%s)? ", oldBoard.content);
