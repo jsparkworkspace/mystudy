82e1f34f08b3ec17029b31d4aaff08c96606d5d4
jsparkworkspace <wnstn_js@naver.com>
2023-12-21 18:17:09 +0900

    20231221 class

--- java-basic/app/src/main/java/com/eomcs/basic/ex02/Exam0142.java
+++ java-basic/app/src/main/java/com/eomcs/basic/ex02/Exam0142.java
@@ -10,6 +10,9 @@ public class Exam0142 {
     // obj의 타입이 Object이기 때문에 Object에 선언한 멤버만 사용할 수 있다.
     // obj가 가리키는 원래 클래스의 메서드를 호출하고 싶다면
     // 다음과 같이 원래 타입으로 형변환하라.
+    // 형변환 하지않으면 컴파일 단계에서 에러가 발생한다.
+    // => obj의 인스턴스는 String 클래스이기 때문에 실행에는 문제 없으나
+    // => 컴파일러는 obj의 타입인 Object 클래스에서 toLowerCase()를 찾는데 메서드가 없어서 에러 발생
     String str = ((String) obj).toLowerCase();
     System.out.println(str);
 
@@ -23,6 +26,5 @@ public class Exam0142 {
     System.out.println(str);
   }
 }
-
 
 
