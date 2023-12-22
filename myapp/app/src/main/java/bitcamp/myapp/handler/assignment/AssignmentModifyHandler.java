099bc9394ad4d876d72da1168029a195c2f8802e
jsparkworkspace <wnstn_js@naver.com>
2023-12-22 15:07:37 +0900

    myapp-25 1

--- myapp/app/src/main/java/bitcamp/myapp/handler/assignment/AssignmentModifyHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/assignment/AssignmentModifyHandler.java
@@ -22,12 +22,16 @@ public class AssignmentModifyHandler extends AbstractMenuHandler {
       System.out.println("과제 번호가 유효하지 않습니다.");
       return;
     }
+    try {
+      Assignment assignment = new Assignment();
+      assignment.setTitle(this.prompt.input("과제명(%s)? ", old.getTitle()));
+      assignment.setContent(this.prompt.input("내용(%s)? ", old.getContent()));
+      assignment.setDeadline(this.prompt.inputDate("제출 마감일(%s)? ", old.getDeadline()));
 
-    Assignment assignment = new Assignment();
-    assignment.setTitle(this.prompt.input("과제명(%s)? ", old.getTitle()));
-    assignment.setContent(this.prompt.input("내용(%s)? ", old.getContent()));
-    assignment.setDeadline(this.prompt.inputDate("제출 마감일(%s)? ", old.getDeadline()));
-
-    this.objectRepository.set(index, assignment);
+      this.objectRepository.set(index, assignment);
+    } catch (Exception e) {
+      System.out.println("과제 변경 오류!");
+      System.out.println("다시 시도 하세요!");
+    }
   }
 }
