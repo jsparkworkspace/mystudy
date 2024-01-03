d7f670d39919ec43d5865a9950c2fb2d6f4e98d5
jsparkworkspace <wnstn_js@naver.com>
2024-01-03 16:20:25 +0900

    myapp-31 5

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -43,6 +43,7 @@ public class App {
     loadAssignment();
     loadBoard();
     loadMember();
+    loadGreeting();
   }
 
   public static void main(String[] args) throws Exception {
@@ -64,6 +65,7 @@ public class App {
     saveAssignment();
     saveBoard();
     saveMember();
+    saveGreeting();
   }
 
   void prepareMenu() {
@@ -333,6 +335,83 @@ public class App {
 
     } catch (Exception e) {
       System.out.println("회원 데이터 로딩 중 오류 발생");
+      e.printStackTrace();
+    }
+  }
+
+  void loadGreeting() {
+    try (FileInputStream in = new FileInputStream("greeting.data")) {
+      byte[] bytes = new byte[60000];
+      int size = in.read() << 8 | in.read();
+
+      for (int i = 0; i < size; i++) {
+        Board board = new Board();
+
+        int len = in.read() << 8 | in.read();
+        in.read(bytes, 0, len);
+        board.setTitle(new String(bytes, 0, len, StandardCharsets.UTF_8));
+
+        len = in.read() << 8 | in.read();
+        in.read(bytes, 0, len);
+        board.setContent(new String(bytes, 0, len, StandardCharsets.UTF_8));
+
+        len = in.read() << 8 | in.read();
+        in.read(bytes, 0, len);
+        board.setWriter(new String(bytes, 0, len, StandardCharsets.UTF_8));
+
+        long date = ((long) in.read() << 56) |
+            ((long) in.read() << 42) |
+            ((long) in.read() << 40) |
+            ((long) in.read() << 32) |
+            ((long) in.read() << 24) |
+            ((long) in.read() << 16) |
+            ((long) in.read() << 8) |
+            in.read();
+        board.setCreatedDate(new java.util.Date(date));
+
+        greetingRepository.add(board);
+      }
+    } catch (Exception e) {
+      System.out.println("가입인사 데이터 로딩 중 오류 발생!");
+      e.printStackTrace();
+    }
+  }
+
+  void saveGreeting() {
+    try (FileOutputStream out = new FileOutputStream("greeting.data")) {
+
+      out.write(greetingRepository.size() >> 8);
+      out.write(greetingRepository.size());
+
+      for (Board board : greetingRepository) {
+        byte[] bytes = board.getTitle().getBytes(StandardCharsets.UTF_8);
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        bytes = board.getContent().getBytes(StandardCharsets.UTF_8);
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        bytes = board.getWriter().getBytes(StandardCharsets.UTF_8);
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        long date = board.getCreatedDate().getTime();
+        out.write((int) (date >> 56));
+        out.write((int) (date >> 48));
+        out.write((int) (date >> 40));
+        out.write((int) (date >> 32));
+        out.write((int) (date >> 24));
+        out.write((int) (date >> 16));
+        out.write((int) (date >> 8));
+        out.write((int) date);
+      }
+
+    } catch (Exception e) {
+      System.out.println("가입인사 데이터 저장 중 오류 발생!");
       e.printStackTrace();
     }
   }
