647de381bfb22c822d623fdb72f5e52b8fed8cbf
jsparkworkspace <wnstn_js@naver.com>
2024-01-10 18:23:09 +0900

    myapp-39 1

---
+++ myapp/app/src/main/java/bitcamp/myapp/dao/DaoTest.java
@@ -0,0 +1,15 @@
+package bitcamp.myapp.dao;
+
+import bitcamp.myapp.vo.Board;
+
+public class DaoTest extends AbstractDao<Board> {
+
+  public static void main(String[] args) {
+    DaoTest obj = new DaoTest();
+    obj.loadData("board.json");
+
+    for (Board item : obj.list) {
+      System.out.println(item);
+    }
+  }
+}
