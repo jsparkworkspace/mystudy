825b4ffb990d1b78f71259c56e8dfe55ccf7fef4
jsparkworkspace <wnstn_js@naver.com>
2024-01-10 16:29:06 +0900

    20240110

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/Main.java
@@ -0,0 +1,17 @@
+package algorithm.test.baekjoon.level05;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+    String b = sc.next();
+
+
+
+  }
+
+}
--- java-basic/app/src/main/java/com/eomcs/openapi/json/jackson/Exam0311.java
+++ java-basic/app/src/main/java/com/eomcs/openapi/json/jackson/Exam0311.java
@@ -16,6 +16,12 @@ public class Exam0311 {
     // GMT 시간인 1970-01-01 로 세터 호출 할 때 getTime()으로 밀리초를 보면 -9시간이 되어있는 것을 확인
     // 즉, 시스템 로케일(Asia/Seoul : GMT+9) 시간 만큼 빠져있다.
     // 세터에서 setTime(0)으로 설정해버리면 jsonStr도 1970-01-01 로 날짜가 동일하게 출력된다.
+
+    // 이말은 세터가 파라미터로 날짜 정보를 받고 저장할 때
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    // 위 포맷에 따라 String으로 저장하는 과정에서 GMT 표준시 기준으로 시간을 반환한다.
+    // 이 때 내 시스템 로케일 시간대인 GMT+9 시간이기 때문에
+    // 표준시로 맞추기 위해 9시간에 대한 밀리초를 뺀다.
     project.setEndDate(Date.valueOf("2024-5-8"));
 
     ObjectMapper mapper = new ObjectMapper();
