8c1d1a69d5742cfc2211cce334a2c69b81554002
jsparkworkspace <wnstn_js@naver.com>
2024-01-12 11:33:06 +0900

    myapp-40 0

---
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/assignment/AssignmentAddHandler.java
@@ -0,0 +1,31 @@
+package bitcamp.myapp.handler.assignment;
+
+import bitcamp.menu.AbstractMenuHandler;
+import bitcamp.myapp.dao.AssignmentDao;
+import bitcamp.myapp.vo.Assignment;
+import bitcamp.util.Prompt;
+
+
+public class AssignmentAddHandler extends AbstractMenuHandler {
+
+  private AssignmentDao assignmentDao;
+
+
+  public AssignmentAddHandler(AssignmentDao assignmentDao, Prompt prompt) {
+    super(prompt);
+    this.assignmentDao = assignmentDao;
+  }
+
+  @Override
+  protected void action() {
+
+    Assignment assignment = new Assignment();
+    assignment.setTitle(this.prompt.input("과제명? "));
+    assignment.setContent(this.prompt.input("내용? "));
+    assignment.setDeadline((this.prompt.inputDate("제출 마감일?(예: 2023-12-25) ")));
+
+    this.assignmentDao.add(assignment);
+
+
+  }
+}
