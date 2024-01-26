9acf94bf71add60690e6bf0aa225ea5191281fdd
jsparkworkspace <wnstn_js@naver.com>
2024-01-26 15:46:56 +0900

    myapp-47 0

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam06/Main.java
@@ -0,0 +1,19 @@
+package algorithm.test.baekjoon.level06.exam06;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    String[] c = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+    for (int i = 0; i < c.length; i++) {
+      if (str.contains(c[i])) {
+        str = str.replace(c[i], "@");
+      }
+      System.out.println(str.length());
+      sc.close();
+    }
+  }
+}
