9feb51cc157a939db4b7298eb67d995a5bcf021e
jsparkworkspace <wnstn_js@naver.com>
2024-02-16 15:02:21 +0900

    myapp-57 2 4

--- myapp/app/src/main/java/bitcamp/myapp/servlet/auth/LogoutServlet.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/auth/LogoutServlet.java
@@ -1,10 +1,10 @@
 package bitcamp.myapp.servlet.auth;
 
-import bitcamp.myapp.servlet.HttpServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
