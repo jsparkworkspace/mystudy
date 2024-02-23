5bc0bfe1e188d89cfa6b978f11f14434ea03235d
jsparkworkspace <wnstn_js@naver.com>
2024-02-23 14:01:43 +0900

    servlet jsp 20240223

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam02/Main.java
@@ -0,0 +1,20 @@
+package algorithm.test.baekjoon.level08.exam02;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+
+    String n = sc.next();
+    int b = sc.nextInt();
+
+    System.out.print(Integer.parseInt(n, b));
+
+    sc.close();
+
+  }
+
+}
