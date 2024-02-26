71b1068ef112bdac577821da14ef595dbd36f3e9
jsparkworkspace <wnstn_js@naver.com>
2024-02-26 11:01:23 +0900

    myapp-61 forward 1

---
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/ErrorServlet.java
@@ -0,0 +1,57 @@
+package bitcamp.myapp.servlet;
+
+import bitcamp.myapp.dao.BoardDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/error")
+public class ErrorServlet extends HttpServlet {
+
+  private BoardDao boardDao;
+
+  @Override
+  public void init() {
+    this.boardDao = (BoardDao) this.getServletContext().getAttribute("boardDao");
+  }
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html lang='en'>");
+    out.println("<head>");
+    out.println("  <meta charset='UTF-8'>");
+    out.println("  <title>비트캠프 데브옵스 5기</title>");
+    out.println("</head>");
+    out.println("<body>");
+
+    request.getRequestDispatcher("/header").include(request, response);
+
+    out.println("<h1>오류!</h1>");
+    String message = (String) request.getAttribute("message");
+    if (message != null) {
+      out.printf("<p>%s</p>\n", message);
+    }
+
+    Throwable exception = (Throwable) request.getAttribute("exception");
+    if (exception != null) {
+      out.println("<pre>");
+      exception.printStackTrace(out);
+      out.println("</pre>");
+    }
+
+    request.getRequestDispatcher("/footer").include(request, response);
+
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
