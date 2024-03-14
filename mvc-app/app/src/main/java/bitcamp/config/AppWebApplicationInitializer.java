baf5d774c14db9255cbefc179de79c245a7fe925
jsparkworkspace <wnstn_js@naver.com>
2024-03-14 16:14:17 +0900

    20240314

---
+++ mvc-app/app/src/main/java/bitcamp/config/AppWebApplicationInitializer.java
@@ -0,0 +1,38 @@
+package bitcamp.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class AppWebApplicationInitializer extends AbstractDispatcherServletInitializer {
+
+  private static Log log = LogFactory.getLog(AppWebApplicationInitializer.class);
+  @Override
+  protected WebApplicationContext createRootApplicationContext() {
+    return null;
+  }
+
+  @Override
+  protected WebApplicationContext createServletApplicationContext() {
+    AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+    appContext.register(AppConfig.class);
+    appContext.refresh();
+    return appContext;
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] {"/app/*"};
+  }
+
+  @Override
+  protected String getServletName() {
+    return "app";
+  }
+
+}
